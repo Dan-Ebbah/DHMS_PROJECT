fa5265229e632eefa6b550a1f6e31fbf6a7f0458
shanmukhatummala <shanmukha.tummala@outlook.com>
2024-04-07 11:19:11 -0400

    add server for FE

---
+++ src/main/java/frontend/FrontEndServer.java
@@ -0,0 +1,17 @@
+package frontend;
+
+import javax.xml.ws.Endpoint;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class FrontEndServer {
+
+    public static void main(String[] args) {
+        Endpoint frontEndServiceEndpoint = Endpoint.publish("http://localhost:8080/frontend", new FrontEndImpl());
+        System.out.println("FrontEnd service is published: " + frontEndServiceEndpoint.isPublished());
+    }
+}
